63e569f5530ed2029b84c6bc0101e8806f58a442
back-sungchae <hiott664477@gmail.com>
2025-12-26 23:17:56 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 30.52 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,21 +1,4 @@
 function solution(s) {
-    const answer = [];
-    const last_index_map = {}; // 문자별 마지막 등장 위치
-
-    for (let i = 0; i < s.length; i++) {
-        const current_char = s[i];
-
-        if (last_index_map[current_char] === undefined) {
-            // 처음 등장
-            answer.push(-1);
-        } else {
-            // 이전 위치와의 거리
-            answer.push(i - last_index_map[current_char]);
-        }
-
-        // 현재 위치로 갱신
-        last_index_map[current_char] = i;
-    }
-
+    var answer = [];
     return answer;
 }
