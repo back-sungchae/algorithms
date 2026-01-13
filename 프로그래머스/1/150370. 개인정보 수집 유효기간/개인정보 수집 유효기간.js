b2112b93b4a97da6c907c09bd530310c3c1768f9
back-sungchae <hiott664477@gmail.com>
2026-01-13 21:58:16 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.64 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,37 @@
+function solution(today, terms, privacies) {
+    var answer = [];
+    let [current_year, current_month, current_day] = today.split(".").map(Number);
+    
+    let terms_obj = {};
+    terms.forEach((value) => {
+      terms_obj[value.split(" ")[0]] = Number(value.split(" ")[1]);
+    });
+
+    for (let i = 0; i < privacies.length; i++) {
+        let [pri_year, pri_month, pri_day] =
+            privacies[i].split(" ")[0].split(".").map(Number);
+        let pri_terms = privacies[i].split(" ")[1];
+
+        // 약관 개월 수 더하기
+        pri_month += terms_obj[pri_terms];
+
+        // 년/월 보정
+        if (pri_month > 12) {
+            pri_year += Math.floor((pri_month - 1) / 12);
+            pri_month = ((pri_month - 1) % 12) + 1;
+        }
+
+        // 🔽 현재 날짜랑 계산된 날짜 비교
+        let expired = pri_year < current_year ||
+            (pri_year === current_year && pri_month < current_month) ||
+            (pri_year === current_year &&
+             pri_month === current_month &&
+             pri_day <= current_day);
+
+        if (expired) {
+            answer.push(i + 1); // 문제 조건: 1번부터 시작
+        }
+    }
+
+    return answer;
+}
